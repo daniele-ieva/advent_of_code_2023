ca40a751d2b09a0082ec6fc4ad5e924497d906c8
Daniele Ieva <daniele.ieva@hotmail.com>
2023-12-04 12:47:58 +0100

    completed day 4 part 1

--- aoc_day_4/part 1/src/main/java/com/adventofcode/Main.java
+++ aoc_day_4/part 1/src/main/java/com/adventofcode/Main.java
@@ -1,7 +1,17 @@
 package com.adventofcode;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws FileNotFoundException {
+        File source = new File("aoc_day_4/part 1/puzzle_input");
+        Scanner scanner = new Scanner(source);
+        int sum = 0;
+        while (scanner.hasNext()) {
+            sum += WinningNumbers.result(scanner.nextLine().split(":")[1]);
+        }
+        System.out.println(sum);
     }
 }
---
+++ aoc_day_4/part 1/src/main/java/com/adventofcode/WinningNumbers.java
@@ -0,0 +1,31 @@
+package com.adventofcode;
+
+import java.util.Arrays;
+
+public class WinningNumbers {
+
+
+    public static int result(String input) {
+        String[] tmp = input.split("\\|");
+        int result = 0;
+        var winning = Arrays.stream(tmp[0].split("\\s+"))
+                .filter(s -> !s.isEmpty())
+                .map(Integer::parseInt).toArray();
+        var numbers = Arrays.stream(tmp[1].split("\\s+"))
+                .filter(s -> !s.isEmpty())
+                .map(Integer::parseInt).toArray();
+
+        for (var win : winning) {
+            for (var num : numbers) {
+                if (win.equals(num)) {
+                    result *= 2;
+                    if (result == 0) {
+                        result = 1;
+                    }
+
+                }
+            }
+        }
+        return result;
+    }
+}
